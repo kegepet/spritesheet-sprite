b2a32c40e8a4cc9c3397b76a27af6939e6570fce
Keith Petrino <kepe@kepe.org>
2018-03-31 18:25:13 -0400

    initial files, nothing to see

---
+++ sss-main.js
@@ -0,0 +1,215 @@
+data = {}
+data.sheets = null // initialize to null so we can check from ui.init()
+data.Sheet = function () { // each element in data.sheets has its prototype set to this
+    this.file = {} // file object to be re-retrieved upon load by a FileReader
+    /*
+    The next three are relational.
+    e.g.
+    a sprite might be a member of several states
+    a frame might be a member of several states
+    a state might be a member of several sprites or frames
+
+    These relationships are stored as ref properties in their respective
+    container objects in the form of indices within the other lists.
+    */
+    this.sprites = [] // all sprites and member data in current sheet
+    this.states = []  // all different states among different sprites 
+    this.frames = []  // all different frames among different states
+    this.grid_distance = 100
+    this.grid_on = true
+    this.guidelines_h = []
+    this.guidelines_v = []
+    this.guidelines_on = true
+}
+data.stringify = function () {
+    // will return the stringified JSON string
+}
+data.add = function (file) {
+    // create new entry in data.sheets, returns ref to new entry
+    var i = data.sheets.push(new data.Sheet())
+    data.sheets[i-1].file = file
+    return data.sheets[i-1]
+}
+data.remove = function (sheet) {
+
+}
+data.update = function (sheet, value=null) {
+
+}
+data.reorder = function () {
+    // the tab order is synced with data.sheets order
+    // when tabs are rearranged, we must reorder data.sheets
+}
+data.init = function () {
+    // create localStorage object if one does not exist
+    // fetch data from localStorage
+    // populate data.sheets
+    if (!('spritesheet-sprite' in localStorage)) {
+        localStorage.setItem('spritesheet-sprite', '')
+        return
+    }
+    try { data.sheets = JSON.parse(localStorage.getItem('spritesheet-sprite')) }
+    catch (err) { data.sheets = [] }
+}()
+
+
+
+
+ui = window.ui || {}
+
+// utility functions
+ui.openAlert = function (template_id, ttl=0) {
+    // ttl set to 0 (secs) will not timeout (default)
+    // clicking on the alert, hitting esc, or the X will close the alert
+    var ov = document.querySelector('template#overlays').content.querySelector('#'+template_id).cloneNode(true)
+    ov.remove = function () {
+        this.className += ' off'
+    }
+    ov.addEventListener('transitionend', function (e) {
+        ov.parentNode.removeChild(ov)
+    })
+    document.body.appendChild(ov)
+    ttl && setTimeout(ov.remove.bind(ov), ttl*1000)
+}
+
+ui.openModal = function (message, choices, callback) {
+    // choices is an array with each item a button value
+    // the callback will be passed the selected index from the choices array
+    var o = document.createElement('div')
+    o.className = 'overlay'
+    var m = document.createElement('div')
+    m.className = 'modal'
+    o.appendChild(m)
+    var mm = document.createElement('div')
+    mm.className = 'message'
+    mm.innerText = message
+    m.appendChild(mm)
+    for (var i=0; i < choices.length; i++) {
+        var b = document.createElement('div')
+        b.className = 'choice'
+        b.choice = i;
+        b.innerText = choices[i]
+        b.addEventListener('click', function (e) {
+            callback(this.choice)
+            o.className += ' off'
+        })
+        m.appendChild(b)
+    }
+    document.body.appendChild(o)
+}
+
+
+// TAB STUFF
+
+ui.newTab = function (file) {
+    // if no file is passed, it will open the default filedrop tab
+    //console.log(file.name)
+    var d = data.add(file)
+    var s = ui.sheet.create(d)
+    document.querySelector('#sheets').appendChild(s)
+    // create new tab
+    var t = document.querySelector('template#tab').content.querySelector('.tab').cloneNode(true)
+    t.data = d
+    // tab and sheet should hold reference to one another
+    t.sheet = s
+    sheet.tab = t
+    t.addEventListener('mousedown', ui.selectTabs)
+    s.setClass = t.setClass = function (classname, tf) {
+        this.className = this.className.replace(new RegExp(' *'+classname, 'g'), '')
+        if (tf) this.className += this.className ? ' '+classname : classname
+    }
+    t.innerText = d.file.name
+    document.querySelector('#tabs').appendChild(t)
+    ui.selectTabs(t)
+}
+
+ui.current = null
+ui.last = null // because ctrl+\ will revert to previous tab
+ui.switchTab = function (tab, skipLast=false) {
+    //console.log(tab.data.file.name)
+    if (ui.current) {
+        ui.current.setClass('current', false)
+        ui.current.sheet.setClass('current', false)
+    }
+    tab.sheet.setClass('current', true)
+    tab.setClass('current', true)
+    ui.last = skipLast ? ui.last : ui.current
+    ui.current = tab
+}
+
+ui.closeTabs = function (tabs) {
+    // takes an array of indices
+    // calls data.update with a value of 0
+    // that causes data.update purge the item
+
+}
+
+ui.anchor = null
+ui.selectTabs = function (x) {
+    // x can be either a MouseEvent or a reference to a tab
+    var me = x.constructor == MouseEvent
+    var target = me ? x.target : x
+    if (document.body.querySelectorAll('.selected').length==1 &&
+        /current/.test(target.className)) return
+    var ck = me && (x.ctrlKey || (/mac/i.test(navigator.platform) && x.metaKey))
+    var sk = me && x.shiftKey
+    var tabs = document.body.querySelectorAll('.tab')
+    if (!me || !ck) {
+        !ck && [].forEach.call(tabs, function (i) {
+            i.setClass('selected', false)
+        })
+    }
+    if (sk) {
+        for (var i=0, sel=0; sel < 2; i++) {
+            sel += tabs[i] == ui.anchor
+            sel += tabs[i] == target
+            sel && tabs[i].setClass('selected', true)
+        }
+    }
+    else ui.anchor = target
+    if (!sk && ck) {
+        target.setClass('selected', !/selected/.test(target.className))
+        if (/current/.test(target.className)) {
+            target = document.body.querySelector('.selected') // left-most selected tab
+        }
+        else if (!/selected/.test(target.className)) return
+    }
+    target.setClass('selected', true)
+    console.log(target.className)
+    ui.switchTab(target, ck || sk)
+}
+
+
+ui.main = document.querySelector('#main')
+ui.main.addEventListener('dragenter', function (e) {
+    ui.openAlert('filedrop')
+})
+ui.main.addEventListener('dragleave', function (e) {
+    document.querySelector('body > .overlay').remove()
+})
+ui.main.addEventListener('dragover', function (e) {// yes, this is necessary
+    e.preventDefault()
+})
+ui.main.addEventListener('drop', function (e) {
+    e.preventDefault()
+    document.querySelector('body > .overlay').remove()
+    if (!e.dataTransfer.files) {
+        return
+    }
+    [].forEach.call(e.dataTransfer.files, function (item) {
+        ui.newTab(item)
+    })
+})
+window.addEventListener('keydown', function (e) {
+    
+    // check reserved
+    if (!/[\[\]\\]/.test(e.key)) return
+    e.preventDefault()
+    //e.stopPropagation()
+    if (!((/mac/i.test(navigator.platform) && e.metaKey) || e.ctrlKey)) return
+
+    if (!ui.current) return
+    if (e.key == '[') ui.selectTabs(ui.current.previousSibling || ui.current.parentNode.lastChild)
+    if (e.key == ']') ui.selectTabs(ui.current.nextSibling || ui.current.parentNode.firstChild)
+    if (e.key == '\\') ui.selectTabs(ui.last)
+})
---
+++ sss-sheet.js
@@ -0,0 +1,41 @@
+ui = window.ui || {}
+
+ui.sheet = {}
+
+// the actual workarea
+ui.sheet.stage = {}
+// shallow-copy the following handlers into the cloned element
+ui.sheet.stage.onclick
+ui.sheet.stage.ondblclick
+ui.sheet.stage.onmousedown
+ui.sheet.stage.onmouseup
+ui.sheet.stage.onmousemove
+ui.sheet.stage.onwheel
+ui.sheet.stage.keydown
+ui.sheet.stage.onkeyup
+
+// the tools panel
+ui.sheet.panel = {}
+ui.sheet.panel.onclick
+ui.sheet.panel.ondblclick
+ui.sheet.panel.onmousedown
+ui.sheet.panel.onmouseup
+ui.sheet.panel.onmousemove
+ui.sheet.panel.onwheel
+ui.sheet.panel.keydown
+ui.sheet.panel.onkeyup
+
+ui.sheet.create = function (dataref) {
+    var sheet = document.querySelector('template#sheet').content.querySelector('.sheet').cloneNode(true)
+    sheet.data = dataref
+    sheet.stage = sheet.querySelector('.stage')
+    for (var i in ui.sheet.stage) {
+        sheet.stage[i] = ui.sheet.stage[i]
+    }
+    sheet.panel = sheet.querySelector('.panel')
+    for (var i in ui.sheet.panel) {
+        sheet.panel[i] = ui.sheet.panel[i]
+    }
+    sheet.stage.innerText = sheet.data.file.name
+    return sheet
+}
